422861493ac493bd919fceba7bedbfd19813a418
IsaacDev14 <mwitiisaac14@gmail.com>
2025-03-17 15:41:42 +0300

    adds a priority option

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
     let form = document.querySelector('form')
     form.addEventListener('submit', (e) =>{
        e.preventDefault();
-       buildToDo(e.target.to_do.value);
+
+       let todoText = document.getElementById("to_do").value;
+       let priority = document.getElementById("priority").value;
+
+       if (todoText.trim() === "") {
+        alert("Task description cannot be empty!");
+        return;
+        }
+
+       buildToDo(todoText, priority);
        form.reset()
     })
 });
 
-function buildToDo(todo){
+function buildToDo(todo, priority){
     let p = document.createElement('p');
     let btn = document.createElement('button');
+
+    if(priority === "high"){
+        p.style.backgroundColor = "green"
+    } else if(priority === "medium"){
+        p.style.backgroundColor = "orange"
+    } else if (priority === "low"){
+        p.style.backgroundColor = "red"
+    };
+
     btn.addEventListener('click', handleDelete);
     btn.textContent = 'delete';
     btn.style.color = "red"
